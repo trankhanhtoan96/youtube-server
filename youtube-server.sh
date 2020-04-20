bf1b1a5d4c61d739798d908561e8c460fac01566
tkt96 <trankhanhtoan96@gmail.com>
2020-04-20 23:26:54 +0700

    tkt

--- youtube-server.sh
+++ youtube-server.sh
@@ -8,6 +8,7 @@ pip3 install pyvirtualdisplay
 apt-get -y install xvfb
 cd /root
 git clone https://trankhanhtoan96:Trankhanhtoan29101996@github.com/trankhanhtoan96/selenium.git
+chmod +x /root/selenium/firefox_driver/geckodriver-linux64
 apt-get -y update
 apt-get -y install vnc4server
 apt-get -y install xfce4 xfce4-goodies
